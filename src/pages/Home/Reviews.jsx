602060fc0dc2561440ccf850d4f7bbb9a93bf895
ayatbd <albehestione@gmail.com>
2024-02-03 20:49:14 +0600

    added review section component

---
+++ src/pages/Home/Reviews.jsx
@@ -0,0 +1,16 @@
+const Reviews = () => {
+  return (
+    <div>
+      <div>
+        <div className="text-center space-y-4">
+          <p className="text-[#D02D77]">Trusted Brand</p>
+          <h2 className="text-4xl text-white font-bold">
+            Trust by 120+ couples
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
