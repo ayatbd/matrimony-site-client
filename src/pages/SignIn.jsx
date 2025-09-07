7bddc20b60d52f12ea26f317443be450ad071c3f
A Behesti <albehestione@gmail.com>
2025-09-07 20:11:04 +0300

    updated login component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -33,7 +33,6 @@ const SignIn = () => {
   const handleLoginWithPopUp = () => {
     signInWithPopup(auth, googleProvider).then((result) => {
       const loggedInUser = result.user;
-      console.log(loggedInUser);
       const saveUser = {
         name: loggedInUser.displayName,
         email: loggedInUser.email,
@@ -120,11 +119,11 @@ const SignIn = () => {
        hover:border-blue-400 focus:bg-blue-50 active:bg-blue-100"
                 >
                   <div className="relative flex items-center space-x-4 justify-center">
-                    <img
-                      src="https://tailus.io/sources/blocks/social/preview/images/google.svg"
+                    {/* <img
+                      src=""
                       className="absolute left-0 w-5"
                       alt="google logo"
-                    />
+                    /> */}
                     <span className="block w-max font-semibold tracking-wide text-[#530E29] text-sm transition duration-300 group-hover:text-blue-600 sm:text-base">
                       Continue with Google
                     </span>
